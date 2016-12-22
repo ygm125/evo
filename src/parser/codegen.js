656d142f762f05ca84e60c0baaecd853e1af9954
guangming.ygm <yuguangming125@126.com>
2016-12-22 23:24:58 +0800

    up

--- src/parser/codegen.js
+++ src/parser/codegen.js
@@ -2,7 +2,25 @@ import { makeFunction } from './helpers'
 
 export default function codeGen(ast) {
 
-    
+    const code = ast ? genElement(ast) : '_c("div")'
 
     return makeFunction(`with(this){return ${code}}`)
+}
+
+function genElement(el) {
+    if (el.for) {
+        return genFor(el)
+    } else if (el.if) {
+        return genIf(el)
+    } else {
+        let code
+        const data = genData(el)
+        const children = genChildren(el, true)
+        code = `_c('${el.tag}'${
+            data ? `,${data}` : '' // data
+            }${
+            children ? `,${children}` : '' // children
+        }`
+        return code
+    }
 }
